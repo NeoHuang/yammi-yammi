c175a6e3203b0f888e16f9b1aa24acf020f6a27b
NeoHuang <superneohuang@gmail.com>
2013-06-26 21:58:06 +0200

    add some changes for bootstrap and index.php

--- www/index.php
+++ www/index.php
@@ -1,3 +1,10 @@
 <?php
-phpinfo();
-?>
+if (version_compare(PHP_VERSION, '5.3.1', '<'))
+{
+	die('Your host needs to use PHP 5.3.1 or higher!');
+}
+
+define('DS', DIRECTORY_SEPARATOR);
+define('ROOT', dirname(dirname(__FILE__)));
+$url = $_GET['url'];
+require_once (ROOT . DS . 'framework' . DS . 'bootstrap.php');
